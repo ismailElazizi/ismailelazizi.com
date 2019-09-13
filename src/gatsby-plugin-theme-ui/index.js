bcdfb47d399b5ee8d9f96eca60640b50d58e9df1
Ismail El Azizi <smailelazizi@gmail.com>
2019-09-13 22:34:58 +0100

    set dark mode as default

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,9 @@
+import novelaTheme from "@narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui";
+
+export default {
+  ...novelaTheme,
+  initialColorMode: `dark`,
+  colors: {
+    ...novelaTheme.colors,
+  },
+};
